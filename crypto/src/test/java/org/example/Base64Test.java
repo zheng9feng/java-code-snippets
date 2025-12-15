71765dd43974aad9315f612a2ca22f4b65b58ec3
m0v1 <mail@atomoperate.com>
2025-12-15 22:04:47 +0800

    test: add URL safe Base64 encoding and decoding tests

--- crypto/src/test/java/org/example/Base64Test.java
+++ crypto/src/test/java/org/example/Base64Test.java
@@ -6,6 +6,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class Base64Test {
 
@@ -30,5 +31,38 @@ public class Base64Test {
         byte[] decodedBytes = Base64.getDecoder().decode(encodedString);
         String decodedString = new String(decodedBytes, StandardCharsets.UTF_8);
         assertEquals("Hello Base64!", decodedString);
+    }
+
+    @Test
+    public void testEncodeUrlSafeBase64() {
+        String originalString = "https://example.com/?q=Base64UrlSafe-Example!";
+
+        byte[] bytesToEncode = originalString.getBytes(StandardCharsets.UTF_8);
+        // --- URL Safe Encoding ---
+        String encodedStringWithUrlEncoder = Base64.getUrlEncoder().encodeToString(bytesToEncode);
+        String encodedStringWithNonUrlEncoder = Base64.getEncoder().encodeToString(bytesToEncode);
+
+        assertEquals("aHR0cHM6Ly9leGFtcGxlLmNvbS8_cT1CYXNlNjRVcmxTYWZlLUV4YW1wbGUh",
+                encodedStringWithUrlEncoder);
+        assertEquals("aHR0cHM6Ly9leGFtcGxlLmNvbS8/cT1CYXNlNjRVcmxTYWZlLUV4YW1wbGUh",
+                encodedStringWithNonUrlEncoder);
+        assertNotEquals(encodedStringWithUrlEncoder, encodedStringWithNonUrlEncoder);
+    }
+
+    @Test
+    public void testDecodeUrlSafeBase64() {
+        String encodedStringWithUrlEncoder = "aHR0cHM6Ly9leGFtcGxlLmNvbS8_cT1CYXNlNjRVcmxTYWZlLUV4YW1wbGUh";
+        String encodedStringWithNonUrlEncoder = "aHR0cHM6Ly9leGFtcGxlLmNvbS8/cT1CYXNlNjRVcmxTYWZlLUV4YW1wbGUh";
+        // --- URL Safe Decoding ---
+        byte[] decodedBytesFromUrlEncoder = Base64.getUrlDecoder().decode(encodedStringWithUrlEncoder);
+        byte[] decodedBytesFromNonUrlEncoder = Base64.getDecoder().decode(encodedStringWithNonUrlEncoder);
+
+        String decodedStringFromUrlEncoder = new String(decodedBytesFromUrlEncoder, StandardCharsets.UTF_8);
+        String decodedStringFromNonUrlEncoder = new String(decodedBytesFromNonUrlEncoder, StandardCharsets.UTF_8);
+
+        assertEquals("https://example.com/?q=Base64UrlSafe-Example!",
+                decodedStringFromUrlEncoder);
+        assertEquals("https://example.com/?q=Base64UrlSafe-Example!",
+                decodedStringFromNonUrlEncoder);
     }
 }
